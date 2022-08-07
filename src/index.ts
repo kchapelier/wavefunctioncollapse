1d4c09b25188c572faf8c4dae8919b0a66cd634c
Rody Davis <rody.davis.jr@gmail.com>
2022-08-06 20:17:26 -0700

    migrating to typescript

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import overlappingModel from './overlapping-model';
+import tiledModel from './simple-tiled-model';
+
+export default {
+  OverlappingModel: overlappingModel,
+  SimpleTiledModel: tiledModel,
+};
